22caba7222023738c897fc8ffcfb9656af7fc5ef
unknown <alanjarosjd@gmail.com>
2021-04-09 10:13:40 +0200

    10:13

---
+++ main.js
@@ -0,0 +1,33 @@
+function imie() {
+    const gora = document.getElementById('imie').value
+    console.log (gora)
+    
+    }
+    function imie() {
+    const imie = document.getElementById('imie').value
+    if(imie.length < 3 ) {
+        alert('za krótkie imię')
+    }
+    
+    }
+    
+    function nazwisko() {
+        const eloe = document.getElementById('nazwisko').value
+        console.log (elo)
+    }
+    
+    function nazwisko() {
+        const imie = document.getElementById('nazwisko').value
+        if(imie.length < 5 ) {
+            alert('za krótkie nazwisko')
+        }
+    }
+    
+    function email(val) {
+    let gv = val.split('')
+    console.log(xd)
+    const toEmail = xd.some(item => item === '@')
+    console.log(toEmail)
+    
+    
+    }
